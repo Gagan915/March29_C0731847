d3fc5b4f61bbbf97cd4b444af5163bdfce7f0915
visualstudio <visualstudio@122-PC12>
2019-03-29 20:06:45 -0400

    ciit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApp1
@@ -10,7 +11,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //
+            {
+                HelloWorld hw = new HelloWorld();
+                hw.Hello();
+            }
+
+        }
+        class HelloWorld
+        {
+            public void Hello()
+            {
+                Console.WriteLine("Hello World");
+
+            }
+        }
+        static void Download()
+        {
+            Thread.Sleep(3000);
+            Console.WriteLine("Download complete");
         }
     }
+    //
 }
